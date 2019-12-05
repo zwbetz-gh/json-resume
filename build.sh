ea278d07ef4c513bd424bccf4e56a3a7dee31f34
zwbetz <zwbetz@gmail.com>
2019-12-04 23:09:32 -0600

    rename index to resme

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# JSON
-cp data/resume.json public/index.json
-
 # HTML
 hugo --gc --cleanDestinationDir
+mv public/index.html public/resume.html
+
+# JSON
+cp data/resume.json public/
 
 # PDF
 hugo server --gc --disableFastRender &
 hugo_server_pid=$!
-wkhtmltopdf http://localhost:1313 public/index.pdf
+wkhtmltopdf http://localhost:1313 public/resume.pdf
 kill $hugo_server_pid
