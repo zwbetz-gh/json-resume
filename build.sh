286755355efce0d3475a5335c50ffdd8e66b3080
zwbetz <zwbetz@gmail.com>
2019-12-05 20:59:25 -0600

    2px border

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ mv public/index.html public/resume.html
 cp data/resume.json public/
 
 # PDF
-hugo server --gc --disableFastRender &
-hugo_server_pid=$!
-wkhtmltopdf http://localhost:1313 public/resume.pdf
+# hugo server --gc --disableFastRender &
+# hugo_server_pid=$!
+# wkhtmltopdf http://localhost:1313 public/resume.pdf
 # sudo google-chrome --no-sandbox --headless --print-to-pdf=public/resume.pdf http://localhost:1313
-kill $hugo_server_pid
+# kill $hugo_server_pid
